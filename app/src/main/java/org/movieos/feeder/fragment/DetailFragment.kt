c6c95f43cdacd1fd6afcada0caf629f13f1111b0
Thomas Insam <tominsam@eventbrite.com>
2017-05-31 22:19:41 -0700

    better back

--- app/src/main/java/org/movieos/feeder/fragment/DetailFragment.kt
+++ app/src/main/java/org/movieos/feeder/fragment/DetailFragment.kt
@@ -8,7 +8,6 @@ import android.support.v4.view.ViewPager
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import io.realm.Realm
-import org.movieos.feeder.R
 import org.movieos.feeder.databinding.DetailFragmentBinding
 import org.movieos.feeder.model.Entry
 import org.movieos.feeder.utilities.Web
@@ -44,8 +43,7 @@ class DetailFragment : DataBindingFragment<DetailFragmentBinding>() {
     override fun createBinding(inflater: LayoutInflater, container: ViewGroup?): DetailFragmentBinding {
         val binding = DetailFragmentBinding.inflate(inflater, container, false)
 
-        binding.toolbar.setNavigationIcon(R.drawable.ic_arrow_back_control_24dp)
-        binding.toolbar.setNavigationOnClickListener { activity.onBackPressed() }
+        binding.toolbarBack.setOnClickListener{  activity.onBackPressed() }
 
         binding.viewPager.adapter = adapter
         binding.viewPager.addOnPageChangeListener(object : ViewPager.OnPageChangeListener {
@@ -85,36 +83,6 @@ class DetailFragment : DataBindingFragment<DetailFragmentBinding>() {
         Timber.i("Selected $entry")
         val binding = binding ?: return
         binding.entry = entry
-
-        // reset menu completely every time, so we can be sure we never generate it in a weird half-state
-        binding.toolbar.menu.clear()
-//        binding.toolbar.inflateMenu(R.menu.detail_menu)
-//        val starred = binding.toolbar.menu.findItem(R.id.menu_star)
-//        val star = ContextCompat.getDrawable(context, if (entry.starred) R.drawable.ic_star_24dp else R.drawable.ic_star_border_24dp)
-//        star.setTint(0xFFFFFFFF.toInt())
-//        starred.icon = star
-
-//        val unread = binding.toolbar.menu.findItem(R.id.menu_unread)
-//        val circle = ContextCompat.getDrawable(context, if (entry.unread) R.drawable.ic_remove_circle_black_24dp else R.drawable.ic_remove_circle_outline_black_24dp)
-//        circle.setTint(0xFFFFFFFF.toInt())
-//        unread.icon = circle
-
-//        binding.toolbar.setOnMenuItemClickListener {
-//            when (it.itemId) {
-//                R.id.menu_star -> Entry.setStarred(context, realm, entry, !entry.starred)
-//                R.id.menu_unread -> Entry.setUnread(context, realm, entry, !entry.unread)
-//                R.id.menu_open -> Web.openInBrowser(activity, entry.url)
-//                R.id.menu_share -> {
-//                    val shareIntent = Intent(Intent.ACTION_SEND)
-//                    shareIntent.type = "text/plain"
-//                    shareIntent.putExtra(Intent.EXTRA_TEXT, entry.url)
-//                    val chooser = Intent.createChooser(shareIntent, null)
-//                    startActivity(chooser)
-//                }
-//            }
-//            true
-//        }
-
         binding.toolbarStarred.setOnClickListener { Entry.setStarred(context, realm, entry, !entry.starred) }
         binding.toolbarUnread.setOnClickListener { Entry.setUnread(context, realm, entry, !entry.unread) }
         binding.toolbarOpen.setOnClickListener { Web.openInBrowser(activity, entry.url) }
