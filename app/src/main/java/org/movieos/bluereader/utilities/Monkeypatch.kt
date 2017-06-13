47d4db088c6a5ae9456d2d94c69885af746b18f0
Thomas Insam <tominsam@eventbrite.com>
2017-06-12 21:34:35 -0700

    utility

--- app/src/main/java/org/movieos/bluereader/utilities/Monkeypatch.kt
+++ app/src/main/java/org/movieos/bluereader/utilities/Monkeypatch.kt
@@ -1,5 +1,15 @@
 package org.movieos.bluereader.utilities
 
+import timber.log.Timber
+
 fun <T> List<T>.sliceSafely(start: Int, end: Int): List<T> {
     return subList(Math.min(start, size), Math.min(end, size))
+}
+
+public inline fun <T> measureTimeMillis(message: String, block: () -> T): T {
+    val start = System.currentTimeMillis()
+    val result = block()
+    val duration = System.currentTimeMillis() - start
+    Timber.i("Timed $message at $duration ms")
+    return result
 }
